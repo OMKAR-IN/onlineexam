5446cd1b746407eba93322a59fffa9aa1a3e135d
Omkar Shinge <oshinge@gmail.com>
2017-06-27 01:46:00 +0530

    Create exam app and define basic test,subject and questions models

---
+++ exam/models.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+class questions(models.Model):
+    que = models.CharField(max_length=255)
+    opt1 = models.CharField(max_length=255)
+    opt2 = models.CharField(max_length=255)
+    opt3 = models.CharField(max_length=255)
+    opt4 = models.CharField(max_length=255)
+    corr_opt = models.CharField(max_length=255)
+    test_que = ForeignKey('exam.tests', related_name=test_questions)
+
+class tests(models.Model):
+    test_name = models.CharField(max_length=255)
+    test_sub = ForeignKey('exam.subjects', related_name='test_subject')
+
+class subjects(models.Model):
+    name = models.CharField(max_length=255)
